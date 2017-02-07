98cc9c0d5fbacfd0b4fbf68ed7ef1ffa85d643b7
koyo <koyo@koyo.kr>
2017-02-07 20:51:07 +0900

    add power, beacon count, channel

--- driver.cpp
+++ driver.cpp
@@ -1,146 +1,150 @@
-#include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 #include <thread>
 #include <mutex>
 #include <atomic>
 #include <map>
 #include <queue>
-#include <algorithm>
 #include <chrono>
 
-#include <tins/packet_sender.h>
 #include <tins/sniffer.h>
 #include <tins/radiotap.h>
 #include <tins/dot11.h>
-#include <tins/tins.h>
+// #include <tins/rsn_information.h>
 
 #include "driver.hpp"
 #include "cli.hpp"
-
-namespace {
-std::mutex g_mtx;
-}
-
-void RequestApInfo(const std::string interface, std::atomic<bool>& stop) {
-    using namespace std::chrono_literals;
     
-    Tins::Dot11Beacon probe;
-    probe.addr1(Tins::Dot11::BROADCAST);
-    probe.addr2("00:01:02:03:04:05");
-    probe.addr3(probe.addr2());
-    
-    probe.ssid("ssid");
-    probe.ds_parameter_set(8);
-    probe.supported_rates({ 1.0f, 5.5f, 11.0f });
-    //probe.rsn_information(Tins::RSNInformation::wpa2_psk());
-    
-    auto tap = Tins::RadioTap() / probe;
-    Tins::PacketSender sender{ interface };
-    
-    while (!stop) {
-        {
-            std::lock_guard<std::mutex> lock{ g_mtx };
-            sender.send(tap);
-            std::this_thread::sleep_for(200ms);
-        }
-        std::this_thread::sleep_for(2000ms);
-    }
-}
-
-struct Info {
-    Tins::Dot11::address_type bssid;
-    int8_t dbm_signal;
+struct Dot11Info {
+    std::string bssid;
+    std::string power;
+    std::string beacons;
+    // std::string data;
+    // std::string per_s;
+    std::string channel;
+    // std::string MB;
+    // std::string ENC;
+    // std::string CIPHER;
+    // std::string AUTH;
     std::string essid;
 };
 
-void UpdateScreen(std::map<Tins::Dot11::address_type, Info>& beacon_map,
-                  std::deque<Info>& probe_deque) {
-    // TODO: map to vector -> SLOW
-    std::vector<Info> beacons;
-    for (const auto& p : beacon_map) {
-        beacons.emplace_back(p.second);
-    }
-    
-    std::sort(beacons.begin(), beacons.end(), [](auto&& a, auto&& b) {
-        return a.dbm_signal < b.dbm_signal;
-    });
-    
-    std::string str;
-    for (const auto& beacon : beacons) {
-        str += ' ' + beacon.bssid.to_string();
-        str += ' ' + std::to_string(beacon.dbm_signal);
-        str += ' ' + beacon.essid;
-        str += '\n';
-    }
-    
-    cli::Update(str);
+namespace {
+    std::mutex g_mutex;
 }
 
-// change: true
-// not change: false
-bool UpdateApInfo(std::map<Tins::Dot11::address_type, Info>& beacon_map,
-                  std::deque<Info>& probe_deque,
-                  Tins::Dot11ManagementFrame& management,
-                  int8_t dbm_signal) {
-    constexpr int kProbeDequeMaxSize = 5;
-    
-    auto init_info = [&]() {
-        return Info{ management.addr2(), dbm_signal, management.ssid() };
-    };
-    
-    switch (management.subtype()) {
-    case Tins::Dot11::PROBE_REQ:
-    case Tins::Dot11::PROBE_RESP:
-        if (probe_deque.size() >= kProbeDequeMaxSize) {
-            probe_deque.pop_front();
-        }
-        probe_deque.emplace_back(init_info());
-        //UpdateScreen(beacon_map, probe_deque);
-        return true;
-    case Tins::Dot11::BEACON:
-        beacon_map[management.addr2()] = init_info();
-        UpdateScreen(beacon_map, probe_deque);
-        return true;
-    default:
-        return false;
-    }
-}
-
-void ReceiveApInfo(const std::string interface, std::atomic<bool>& stop) {
+void PrintInfo(const std::map<std::string, Dot11Info>& info_map,
+               const std::deque<Dot11Info>& info_deque,
+               const std::atomic<bool>& stop) {
     using namespace std::chrono_literals;
     
-    std::map<Tins::Dot11::address_type, Info> beacon_map;
-    std::deque<Info> probe_deque;
+    std::stringstream msg;
+    const std::string str{ "  BSSID            "
+                           "  PWR"
+                           "  Beacons"
+                           "  CH"
+                           "  ESSID\n" };
+    
+    while (!stop) {
+        msg.str(std::string());
+        msg << str;
+        
+        std::this_thread::sleep_for(200ms);
+        {
+            std::lock_guard<std::mutex> lock{ g_mutex };
+            for (const auto& p : info_map) {
+                const auto& info = p.second;
+                msg << std::setw(2 + 17) << info.bssid
+                    << std::setw(2 + 3)  << info.power
+                    << std::setw(2 + 7)  << info.beacons
+                    << std::setw(2 + 2)  << info.channel
+                    << "  " << info.essid << "\n";
+            }
+        }
+        cli::Update(msg.str());
+    }
+}
+
+// change -> true
+// not change -> false
+bool UpdateInfo(std::map<std::string, Dot11Info>& info_map,
+                std::deque<Dot11Info>& info_deque,
+                const Tins::RadioTap& tap,
+                const Tins::Dot11ManagementFrame& mgmt) {
+    auto init_info = [&](Dot11Info& info) {
+        auto freq = tap.channel_freq();
+        auto&& ssid = mgmt.ssid();
+        
+        std::lock_guard<std::mutex> lock{ g_mutex };
+        info.bssid = mgmt.addr2().to_string();
+        info.beacons = info.beacons.empty() ?
+                       std::to_string(1) : std::to_string(std::stoi(info.beacons) + 1);
+        info.power = std::to_string(tap.dbm_signal());
+        info.channel = std::to_string(freq == 2484 ? 14 : freq % 2412 / 5 + 1);
+        info.essid = ssid.empty() ? "<length:  0>" : ssid;
+    };
+    
+    switch (mgmt.subtype()) {
+    case Tins::Dot11::PROBE_REQ:
+    case Tins::Dot11::PROBE_RESP:
+        break;
+    case Tins::Dot11::BEACON:
+        try {
+            init_info(info_map[mgmt.addr2().to_string()]);
+        }
+        catch (Tins::option_not_found&) {
+            return false;
+        }
+        break;
+    default:
+        break;
+    }
+    
+    return true;
+}
+
+void ReceiveInfo(const std::string interface,
+                 const std::atomic<bool>& stop) {
+    using namespace std::chrono_literals;
+    std::map<std::string, Dot11Info> info_map;
+    std::deque<Dot11Info> info_deque;
     
     Tins::SnifferConfiguration config;
     config.set_promisc_mode(true);
     Tins::Sniffer sniffer{ interface, config };
     
     cli::NextScreen();
+    std::thread print_thread{ PrintInfo,
+                              std::ref(info_map),
+                              std::ref(info_deque),
+                              std::ref(stop) };
+    
     while (!stop) {
-        std::lock_guard<std::mutex> lock{ g_mtx };
-        sniffer.sniff_loop([&beacon_map, &probe_deque](Tins::PDU& pdu) {
+        sniffer.sniff_loop([&info_map, &info_deque](Tins::PDU& pdu) {
             auto tap = pdu.rfind_pdu<Tins::RadioTap>();
-            return !UpdateApInfo(beacon_map,
-                                 probe_deque,
-                                 tap.rfind_pdu<Tins::Dot11ManagementFrame>(),
-                                 tap.dbm_signal());
-        });
-        std::this_thread::sleep_for(200ms);
+            return !UpdateInfo(info_map,
+                               info_deque,
+                               tap,
+                               tap.rfind_pdu<Tins::Dot11ManagementFrame>());
+        }, 10'000);
+        std::this_thread::sleep_for(10ms);
     }
+    
+    print_thread.join();
 }
 
 int Driver(const std::string interface) {
     std::atomic<bool> stop{ false };
-    std::thread request_thread{ RequestApInfo, interface, std::ref(stop) };
-    std::thread update_thread{ ReceiveApInfo, interface, std::ref(stop) };
+    std::thread recive_thread{ ReceiveInfo,
+                               interface,
+                               std::ref(stop) };
     
-    std::string str;
-    while(std::getline(std::cin, str));
+    std::string input;
+    while(std::getline(std::cin, input));
     
     stop = true;
-    request_thread.join();
-    update_thread.join();
+    recive_thread.join();
     
     return 0;
 }
